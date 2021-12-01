457c96261ff374b88be135fea6d7cce6e79b9955
houssemriden <cloud_user@ec75a065371c.mylabserver.com>
2021-12-01 05:02:43 +0000

    fixed pcapdownload

--- pcapdownload_copy.sh
+++ pcapdownload_copy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
+
+
 echo " Please provide a valid url for pcap file (smallFlows.pcap default) "
 
-read u
+read i
 
+if [ "$i" == "" ] ; then ansible-playbook fourth.yml -i /home/cloud_user/ansible/test.inv --ask-become-pass -e "ur=https://s3.amazonaws.com/tcpreplay-pcap-files/smallFlows.pcap "
+else 
+ ansible-playbook fourth.yml -i /home/cloud_user/ansible/test.inv --ask-become-pass -e "ur=$i"
 
-if [ "$u" == "" ] ; then 
-ansible-playbook fourth.yml -i /home/cloud_user/ansible/test.inv --ask-become-pass -e "ur=https://s3.amazonaws.com/tcpreplay-pcap-files/smallFlows.pcap"
-else
-	ansible-playbook fourth.yml -i /home/cloud_user/ansible/test.inv --ask-become-pass -e "ur=$u"
 fi
